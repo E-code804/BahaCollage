6dc232330d8c5b0863f547f2a8051b1964cba232
Grey <71736626+E-code804@users.noreply.github.com>
2022-05-23 16:05:49 -0400

    Changing the interval of fadeIn/Out and setTimeout

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-let imgString = "./images/baha", imgNum = 2;
+let imgString = "./images/baha", imgNum = 2, interval = 800;
 
 // setInteral has 2 parameters: 
 // The first is a function and the second is time (ms) that the program will wait 
 // until it executes the function.
 setInterval(() => {
-    $("img").fadeOut(1000); // Fade out the current image.
+    $("img").fadeOut(interval); // Fade out the current image.
     setTimeout(() => { // setTimeout is the same as setInterval, but only executes its code once. 
-        $("img").attr("src", imgString + (imgNum++ % 34) + ".JPG").fadeIn(1000);
-    }, 1000);
-}, 4000);
+        $("img").attr("src", imgString + (imgNum++ % 34) + ".JPG").fadeIn(interval);
+    }, interval);
+}, 5250);
